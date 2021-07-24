bfa368d6709b8cb0e40bf72c988d08e2f9dc96d3
davaa <davaa.skywalker@gmail.com>
2021-07-24 15:00:00 +0800

    Toglogchiin eeljiig solih bolon shoo hayah heseg

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 
 
 // Тоглогчийн цуцлуулсан оноог хадгалах хувьсагч
@@ -10,22 +10,53 @@ var scores = [0, 0];
 var roundScore = 0;
 // Шоо аль нүдээр буусаныг хадгалах хувьсагч
 
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-// window.document.querySelector('#score-0').textContent = dice;
-
-// document.querySelector('#score-1').textContent = dice;
-
-console.log("Шоо :" + dice);
 
 
 // Программ эхлэхэд бэлдэнэ
-document.querySelector(".dice").style.display = "none";
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
+document.getElementById("score-0").textContent = "0"
+document.getElementById("score-1").textContent = "0"
+document.getElementById("current-0").textContent = "0"
+document.getElementById("current-1").textContent = "0"
+
+var diceDom=document.querySelector(".dice");
 
 
-window.document.querySelector('#current-0').textContent = 0;
-window.document.querySelector('#current-1').textContent = 0;
+diceDom.style.display = "none";
+// Шоог шидэх эвент листенер
+document.querySelector(".btn-roll").addEventListener("click", function (){
+    // 1-6 хүртэлх санамсаргүй тоо авах
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    // буусан шоог вэб дээр гаргаж ирнэ
+    diceDom.style.display = "block"
+    diceDom.src = 'dice-'+ diceNumber+'.png';
+
+    // буусан тоо нь 1-ээс ялгаатай бол идэвхтэй тоглогчийн оноон дээр нэмнэ.
+
+    if(diceNumber!==1){
+        roundScore = roundScore+diceNumber;
+        document.getElementById("current-"+ activePlayer).textContent = roundScore;
+    }else{
+        // тоглогчийн ээлжийн оноог 0 болгоно
+        roundScore= 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        
+        // тоглогчийн ээлжийг шилжүүлнэ
+        activePlayer === 0 ? (activePlayer = 1): (activePlayer = 0);
+
+
+        diceDom.style.display = "none";
+        // улаан цэгийг шилжүүлнэ
+    document.querySelector(".player-0-panel").classList.toggle('active');
+    document.querySelector(".player-1-panel").classList.toggle('active')
+    }
+    
+
+});
+
+// Тоглогч оноог хадгалахыг хүссэн үед тоглогчийн нийт оноон дээр нэмэх
+
 
 
